17ce87b345df486279196f535b800bfc9b8b05e7
Ariq Khoiri <qq.khoiri@gmail.com>
2022-10-11 10:02:50 +0700

    feat:(Add new kanojo and admin)

--- src/main/java/com/coffekyun/report/security/ApplicationSecurityConfig.java
+++ src/main/java/com/coffekyun/report/security/ApplicationSecurityConfig.java
@@ -2,14 +2,25 @@ package com.coffekyun.report.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig  {
+
+    private PasswordEncoderConfiguration passwordEncoderConfiguration;
+
+    public ApplicationSecurityConfig(PasswordEncoderConfiguration passwordEncoderConfiguration) {
+        this.passwordEncoderConfiguration = passwordEncoderConfiguration;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,4 +36,22 @@ public class ApplicationSecurityConfig  {
                 .build();
     }
 
+    @Bean
+    public UserDetailsService userConfiguration() {
+        UserDetails userKanojo = User.builder()
+                .username("kaguya")
+                .password(passwordEncoderConfiguration.passwordEncoder().encode("kanojo"))
+                .roles("KANOJO")
+                .build();
+
+        UserDetails userAdmin= User.builder()
+                .username("hikaru")
+                .password(passwordEncoderConfiguration.passwordEncoder().encode("kyun"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                userKanojo, userAdmin
+        );
+    }
 }
