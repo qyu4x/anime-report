61cf6cdd47773f6305ab9437267aeee5c3b4c61c
Ariq Khoiri <qq.khoiri@gmail.com>
2022-10-12 20:23:54 +0700

    Add change for parameter request login, logout and remember me

--- src/main/java/com/coffekyun/report/security/ApplicationSecurityConfig.java
+++ src/main/java/com/coffekyun/report/security/ApplicationSecurityConfig.java
@@ -44,16 +44,20 @@ public class ApplicationSecurityConfig  {
                 .and()
                 //.httpBasic()
                 .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/welcome", true)
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/welcome", true)
+                    .usernameParameter("username") // if you want to do customization, for form param request
+                    .passwordParameter("password")
                 .and()
                 .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30)) // default is 2 weeks, but this settig is 30 day
+                    .key("x-secure-nya")
+                    .rememberMeParameter("remember-me")
                 .and()
                 .logout()
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // use this, if csrf is disable, to secure our api, remove this line , if yours csrf is enable(default), because method is post not get
-                .clearAuthentication(true)
-                .deleteCookies("remember-me", "JSESSIONID").logoutSuccessUrl("/login")
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // use this, if csrf is disable, to secure our api, remove this line , if yours csrf is enable(default), because method is post not get
+                    .clearAuthentication(true)
+                    .deleteCookies("remember-me", "JSESSIONID").logoutSuccessUrl("/login")
                 .and()
                 .build();
     }
